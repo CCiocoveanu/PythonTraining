4b190ab5b849d0969881f655f8666931bf60a062
Constantin Ciocoveanu <cciocoveanu@endava.com>
2019-07-24 18:54:24 +0300

    collections -> range

---
+++ tut/not_real_collections.py
@@ -0,0 +1,48 @@
+"""
+str | list | dict | tuple | range | set
+"""
+
+
+def tuple_ex():  # tuple - immutable, cannot be replaced/removed, new elem cannot be added
+    t = ("Norway", 4.93, 3)
+    t_len = len(t)
+    for item in t:
+        print(item)
+    print(t_len)
+
+
+tuple(["This", "is", "a", "str", "list"])  # tuple constructor
+bool_var = 5 in (1, 4, 9, 5, 10)
+bool_var_2 = "a" not in tuple("transforms_in_char")
+
+
+def minmax(items):  # returns a tuple
+    return min(items), max(items)
+
+
+lower, upper = minmax([2, 3, 4, 5, 3, 3, ])
+
+
+def str_ex():  # str - immutable
+    len_str = len("ajshdkjhdfksdhfkjshd")
+    s = "New"
+    s += "land"
+    colors = ';'.join(['#123', '#321', '#030'])  # call join on separator
+    colors.split(';')  # splits into a list
+    "unforgettable".partition("forget")  # returns a tuple  ('un', 'forget' , 'able')
+    "The age of {0} is {1}".format('Jim', 32)
+
+    import math
+    print("Math constants: pi={m.pi}, e={m.e}".format(m=math))
+
+
+def range_ex():# range =  arithmetic progression of integers
+    
+
+
+
+if __name__ == '__main__':
+    print(minmax([1, 2, 3, 4, 6, 8, 30, 1, 0, -39]))
+    str_ex()
+
+
--- tut/objects.py
+++ tut/objects.py
@@ -1,3 +1,6 @@
+import tut.pluralsight
+
+
 def banner(border='-'):
     message = input("Type stuff: ")
     line = border * len(message)
@@ -6,5 +9,39 @@ def banner(border='-'):
     print(line)
 
 
+def get_type():
+    print(tut.pluralsight.__name__)
+
+
+def get_type(arg):
+    print(type(arg))
+
+
 if __name__ == "__main__":
     banner("-")
+    get_type()
+
+
+# variabilele sunt referinte catre objects
+# scopes:
+#   Local - inside the current function
+#   Enclosing - any and all enclosing functions
+#   Global - top level of module
+#   built-in
+
+'''
+named references to objects
+id() returns unique constant identifier
+is determines equality
+== determines equivalence
+function arguments are passed by reference
+reference is lost if a formal function argument is rebound
+default arguments evaluated ones when def is executed
+dynamic typing
+global references ca be read from local scome - global keyword
+everything is an object
+import and def result in binding to maned references
+type() can be used to determine object tyoe
+dir() - introspect an object and get its attributes
+
+'''
